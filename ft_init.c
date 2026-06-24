ba415b58f4aafcf216d6f2fc097bf0b4427a789c
William Lu <william.lu.bjornestad@gmail.com>
2026-06-24 15:18:26 +0200

    Inicialización separada

---
+++ ft_init.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgarci <antgarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 12:00:00 by antgarci          #+#    #+#             */
+/*   Updated: 2026/06/23 12:00:00 by antgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	init_ps(t_ps *ps)
+{
+	int	i;
+
+	i = 0;
+	ps->a = NULL;
+	ps->b = NULL;
+	while (i < 11)
+		ps->counts[i++] = 0;
+}
+
+void	error_exit(t_ps *ps)
+{
+	ft_putstr_fd("Error\n", 2);
+	free_stack(&ps->a);
+	free_stack(&ps->b);
+	exit(1);
+}
+
+int	dup_controller(t_list *stack)
+{
+	t_list	*current;
+	t_list	*checker;
+
+	current = stack;
+	while (current->next != stack)
+	{
+		checker = current->next;
+		while (checker != stack)
+		{
+			if (checker->num == current->num)
+				return (1);
+			checker = checker->next;
+		}
+		current = current->next;
+	}
+	return (0);
+}
+
+void	build_stack(t_ps *ps, char **argv, int start)
+{
+	int	i;
+	int	val;
+	int	err;
+
+	i = start;
+	while (argv[i])
+	{
+		val = ft_atoi(argv[i], &err);
+		if (err)
+			error_exit(ps);
+		lstadd_back(&ps->a, ft_newlst(val));
+		i++;
+	}
+	if (ps->a && dup_controller(ps->a))
+		error_exit(ps);
+}
