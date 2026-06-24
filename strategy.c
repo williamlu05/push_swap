74eedf69afebccaacb77c68fc084174c7e8b3fc9
William Lu <william.lu.bjornestad@gmail.com>
2026-06-24 15:14:15 +0200

    Flags añadidas para selección de algoritmo, y benchmark mensaje

---
+++ strategy.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strategy.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgarci <antgarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 12:00:00 by antgarci          #+#    #+#             */
+/*   Updated: 2026/06/23 12:00:00 by antgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	adaptive_sort(t_ps *ps, double disorder)
+{
+	if (disorder < 0.2)
+		bubble(ps);
+	else if (disorder < 0.5)
+		chunk_sort(ps);
+	else
+		mergesort(ps);
+}
+
+void	run_strategy(t_ps *ps, t_opts *opts, double disorder)
+{
+	if (opts->strategy == S_SIMPLE)
+		bubble(ps);
+	else if (opts->strategy == S_MEDIUM)
+		chunk_sort(ps);
+	else if (opts->strategy == S_COMPLEX)
+		mergesort(ps);
+	else
+		adaptive_sort(ps, disorder);
+}
+
+static char	*adaptive_name(double disorder)
+{
+	if (disorder < 0.2)
+		return ("Adaptive / O(n)");
+	if (disorder < 0.5)
+		return ("Adaptive / O(n*sqrt(n))");
+	return ("Adaptive / O(n log n)");
+}
+
+char	*strategy_name(int strategy, double disorder)
+{
+	if (strategy == S_SIMPLE)
+		return ("Simple / O(n^2)");
+	if (strategy == S_MEDIUM)
+		return ("Medium / O(n*sqrt(n))");
+	if (strategy == S_COMPLEX)
+		return ("Complex / O(n log n)");
+	return (adaptive_name(disorder));
+}
