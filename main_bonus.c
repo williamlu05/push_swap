1ec9b2493013417f5ca314b96552ff06ccdae707
William Lu <william.lu.bjornestad@gmail.com>
2026-06-27 16:20:19 +0200

    Primera implementacion del bonus

---
+++ main_bonus.c
@@ -0,0 +1,70 @@
+
+
+#include "bonus.h"
+
+static int	try_shift(t_ps *ps, char *line)
+{
+	if (!ft_strncmp(line, "sa\n", 4))
+		sa(ps);
+	else if (!ft_strncmp(line, "sb\n", 4))
+		sb(ps);
+	else if (!ft_strncmp(line, "ss\n", 4))
+		ss(ps);
+	else if (!ft_strncmp(line, "pa\n", 4))
+		pa(ps);
+	else if (!ft_strncmp(line, "pb\n", 4))
+		pb(ps);
+	else
+		return (0);
+	return (1);
+}
+
+static int	try_rotate(t_ps *ps, char *line)
+{
+	if (!ft_strncmp(line, "ra\n", 4))
+		ra(ps);
+	else if (!ft_strncmp(line, "rb\n", 4))
+		rb(ps);
+	else if (!ft_strncmp(line, "rr\n", 4))
+		rr(ps);
+	else if (!ft_strncmp(line, "rra\n", 4))
+		rra(ps);
+	else if (!ft_strncmp(line, "rrb\n", 4))
+		rrb(ps);
+	else if (!ft_strncmp(line, "rrr\n", 4))
+		rrr(ps);
+	else
+		return (0);
+	return (1);
+}
+
+void	choose_movement(t_ps *ps, char *line)
+{
+	if (try_shift(ps, line) || try_rotate(ps, line))
+		return ;
+	error_exit(ps);
+}
+
+int main(int argc, char **argv)
+{
+	t_ps	ps;
+	char	*line;
+
+	if (argc < 2)
+		return (0);
+	init_ps(&ps);
+	build_stack(&ps, argv, 1);
+
+	line = get_next_line(1);
+	while (line)
+	{
+		choose_movement(&ps, line);
+		line = get_next_line(1);
+	}
+
+	if (is_sorted(ps.a) && !ps.b)
+		ft_printf("OK");
+	else
+		ft_printf("KO");
+	return (0);
+}
