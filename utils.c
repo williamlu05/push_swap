d0f8e365b63c970f43e53663ba4cb6a10d36c73f
William Lu <william.lu.bjornestad@gmail.com>
2026-06-29 00:14:48 +0200

    Comentario eliminado

--- utils.c
+++ utils.c
@@ -41,7 +41,6 @@ int	ft_atoi(const char *str, int *err)
 	return ((int)(num * sign));
 }
 
-// Returns 0 if two strings equals, difference of content of unequal
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
